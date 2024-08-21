6bdd20cd4fe44453f169ae154380e3929e7d11b3
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-08-21 17:39:15 -0300

    AULA 3

--- strem/src/main/java/com/alura/pix/strem/PixAggregator.java
+++ strem/src/main/java/com/alura/pix/strem/PixAggregator.java
@@ -1,15 +1,32 @@
 package com.alura.pix.strem;
 
 import com.alura.pix.dto.PixDTO;
+import com.alura.pix.serdes.PixSerdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.kafka.common.serialization.Serdes;
+
 
 @Service
 public class PixAggregator {
     @Autowired
     public void aggregator(StreamsBuilder builder){
-        KStream<String, PixDTO>
+
+        KTable<String, Double> stream = builder
+                .stream("pix-topic", Consumed.with(Serdes.String(), PixSerdes.serdes()))
+                .peek((key,value)-> System.out.println("PIX RECEBIDO "+ value.getChaveOrigem()))
+                .groupBy((key,value)->value.getChaveOrigem())
+                .aggregate(
+                        ()->0.0,(key,velue,aggregate)->(aggregate + velue.getValor()),
+                        Materialized.with(Serdes.String(),Serdes.Double())
+
+                )
+                ;
+
+        stream.toStream().print(Printed.toSysOut());
+        stream.toStream().to("pix-topic-validação", Produced.with(Serdes.String(), Serdes.Double()));
+
     }
 }
