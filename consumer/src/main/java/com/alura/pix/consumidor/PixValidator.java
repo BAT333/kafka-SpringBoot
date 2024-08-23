b8dcf4e5ae0f027f63c02b76544284ade7462848
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-08-23 00:30:32 -0300

    AULA 5

--- consumer/src/main/java/com/alura/pix/consumidor/PixValidator.java
+++ consumer/src/main/java/com/alura/pix/consumidor/PixValidator.java
@@ -8,6 +8,9 @@ import com.alura.pix.model.Key;
 import com.alura.pix.model.Pix;
 import com.alura.pix.repository.KeyRepository;
 import com.alura.pix.repository.PixRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.avro.generic.GenericData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
@@ -24,7 +27,7 @@ public class PixValidator {
     @Autowired
     private PixRepository pixRepository;
 
-    @KafkaListener(topics = "pix-topic", groupId = "grupo")
+    @KafkaListener(topics = "pix-service.public.pix", groupId = "grupo")
     @RetryableTopic(
             backoff = @Backoff(value = 3000L),
             attempts = "4",
@@ -32,22 +35,25 @@ public class PixValidator {
             include = KeyNotFoundException.class
 
     )
-    public void processaPix(PixRecord pixDTO, Acknowledgment acknowledgment) {
+    public void processaPix(GenericData .Record data, Acknowledgment acknowledgment) throws JsonProcessingException {
         acknowledgment.acknowledge();
-        System.out.println("Pix  recebido: " + pixDTO.getIdentificador());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
 
-        Pix pix = pixRepository.findByIdentifier(pixDTO.getIdentificador().toString());
+        PixDTO pixDTO = objectMapper.readValue(data.get("after").toString(), PixDTO.class);
+        if (pixDTO.getStatus().equals(PixStatus.EM_PROCESSAMENTO)){
+            Pix pix = pixRepository.findByIdentifier(pixDTO.getIdentifier());
 
-        Key origem = keyRepository.findByChave(pixDTO.getChaveOrigem().toString());
-        Key destino = keyRepository.findByChave(pixDTO.getChaveDestino().toString());
+            Key origem = keyRepository.findByChave(pixDTO.getChaveOrigem());
+            Key destino = keyRepository.findByChave(pixDTO.getChaveDestino());
 
-        if (origem == null || destino == null) {
-            pix.setStatus(PixStatus.ERRO);
-            throw new KeyNotFoundException();
-        } else {
-            pix.setStatus(PixStatus.PROCESSADO);
+            if (origem == null || destino == null) {
+                pix.setStatus (PixStatus.ERRO);
+            } else {
+                pix.setStatus (PixStatus.PROCESSADO);
+            }
+            pixRepository.save(pix);
         }
-        pixRepository.save(pix);
     }
 
 }
--- consumer/src/main/java/com/alura/pix/dto/PixDTO.java
+++ consumer/src/main/java/com/alura/pix/dto/PixDTO.java
@@ -1,19 +1,25 @@
 package com.alura.pix.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class PixDTO {
+    private Integer id;
     private String identifier;
+    @JsonProperty("chave_origem")
     private String chaveOrigem;
+    @JsonProperty("chave_destino")
     private String chaveDestino;
     private Double valor;
-    private LocalDateTime dataTransferencia;
+    @JsonProperty("data_transferencia")
+    private Date dataTransferencia;
     private PixStatus status;
 }
